a5d0d1ab13fb68f0c4aa2bb7d956a68aac0f24ef
nicolas <nicolas.renard.33@gmail.com>
2023-03-26 10:25:43 +0200

    animation fix

--- src/app/animations/folder.animation.ts
+++ src/app/animations/folder.animation.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/animations';
 
 export const folderAnimation = trigger('folder', [
-  state('hidden', style({ height: '0', overflow: 'hidden', opacity: 0 })),
+  state('hidden', style({ height: '0', opacity: 0 })),
   state('visible', style({ height: '*', opacity: 1 })),
   transition('hidden => visible', animate('300ms ease-out')),
   transition('visible => hidden', animate('300ms ease-out')),
